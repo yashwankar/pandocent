5c591660662c84c1a0c95992a3ffa5c57e46fd29
Yash Wankar <yashwankar25@gmail.com>
2020-10-28 01:58:44 +0530

    added three dots for side navigation

--- app/src/main/java/com/kyrs/pandocent/HomepageActivity.kt
+++ app/src/main/java/com/kyrs/pandocent/HomepageActivity.kt
@@ -1,6 +1,7 @@
 package com.kyrs.pandocent
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -11,20 +12,21 @@ import com.kyrs.pandocent.R
 import kotlinx.android.synthetic.main.homepage.*
 
 
-class HomepageActivity() : AppCompatActivity() {
+class HomepageActivity() : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-
-    //    private var toolbar = findViewById<Toolbar>(R.id.homepage_toolbar)
-//    private var navview = findViewById<NavigationView>(R.id.nav_view)
-//    private var drawer = findViewById<DrawerLayout>(R.id.side_drawer)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homepage)
 
         setSupportActionBar(homepage_toolbar)
-        val toggle= ActionBarDrawerToggle(this, side_drawer, R.string.openNavDrawer, R.string.closeNavDrawer)
+        val toggle= ActionBarDrawerToggle(this, side_drawer,homepage_toolbar, R.string.openNavDrawer, R.string.closeNavDrawer)
         side_drawer.addDrawerListener(toggle)
         toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
 
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return false
     }
 }
