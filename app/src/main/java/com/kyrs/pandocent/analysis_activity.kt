9d07afec39dbc2b0f7a97ffb7f62b3d4f9594e32
ketki <ketki.patil2019@vitbhopal.ac.in>
2020-10-29 00:01:17 +0530

    .

---
+++ app/src/main/java/com/kyrs/pandocent/analysis_activity.kt
@@ -0,0 +1,11 @@
+package com.kyrs.pandocent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class analysis_activity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.analysis_layout)
+}
+}
--- app/src/main/java/com/kyrs/pandocent/health_activity.kt
+++ app/src/main/java/com/kyrs/pandocent/health_activity.kt
@@ -1,13 +1,21 @@
 package com.kyrs.pandocent
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.health_layout.*
+import kotlinx.android.synthetic.main.homepage.*
 
 class health_activity:AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.health_layout)
+
+        quiz.setOnClickListener(){
+            val intent = Intent(this,analysis_activity::class.java)
+            startActivity(intent)
+        }
     }
     }
 
