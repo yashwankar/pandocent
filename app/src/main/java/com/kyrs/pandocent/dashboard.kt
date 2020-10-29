1285b347d9557400e62beaf5e53a18065ad0e8e2
Yash Wankar <yashwankar25@gmail.com>
2020-10-29 11:39:52 +0530

    added location with maps

--- app/src/main/java/com/kyrs/pandocent/dashboard.kt
+++ app/src/main/java/com/kyrs/pandocent/dashboard.kt
@@ -19,22 +19,39 @@ class dashboard() : AppCompatActivity(), NavigationView.OnNavigationItemSelected
         setContentView(R.layout.homepage)
 
         setSupportActionBar(homepage_toolbar)
-        val toggle= ActionBarDrawerToggle(this, side_drawer,homepage_toolbar, R.string.openNavDrawer, R.string.closeNavDrawer)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            side_drawer,
+            homepage_toolbar,
+            R.string.openNavDrawer,
+            R.string.closeNavDrawer
+        )
         side_drawer.addDrawerListener(toggle)
         toggle.syncState()
         nav_view.setNavigationItemSelectedListener(this)
-        safe_mode.setOnClickListener{
-            Toast.makeText(this,"You are SAFE",Toast.LENGTH_LONG).show()
+        safe_mode.setOnClickListener {
+            Toast.makeText(this, "You are SAFE", Toast.LENGTH_LONG).show()
         }
-        textView7.setOnClickListener{
-            Toast.makeText(this,"You are SAFE",Toast.LENGTH_LONG).show()
+        textView7.setOnClickListener {
+            Toast.makeText(this, "You are SAFE", Toast.LENGTH_LONG).show()
         }
 
-        rating.setOnClickListener{
-            Toast.makeText(this,"Your current rating is 80",Toast.LENGTH_LONG).show()
+        location.setOnClickListener {
+            val gmmIntentUri = Uri.parse("geo:23.0775365,76.8490828")
+
+// Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+// Make the Intent explicit by setting the Google Maps package
+            mapIntent.setPackage("com.google.android.apps.maps")
+
+// Attempt to start an activity that can handle the Intent
+            startActivity(mapIntent)
         }
-        health.setOnClickListener(){
-            val intent = Intent(this,health_activity::class.java)
+        rating.setOnClickListener {
+            Toast.makeText(this, "Your current rating is 80", Toast.LENGTH_LONG).show()
+        }
+        health.setOnClickListener() {
+            val intent = Intent(this, health_activity::class.java)
             startActivity(intent)
         }
 
@@ -44,6 +61,7 @@ class dashboard() : AppCompatActivity(), NavigationView.OnNavigationItemSelected
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         return false
     }
+
     override fun onBackPressed() {
         if (side_drawer.isDrawerOpen(GravityCompat.START)) {
             side_drawer.closeDrawer(GravityCompat.START)
@@ -53,13 +71,13 @@ class dashboard() : AppCompatActivity(), NavigationView.OnNavigationItemSelected
     }
 
     fun openstats(view: View) {
-        var uri=Uri.parse("https://www.covid19india.org/")
-        startActivity(Intent(Intent.ACTION_VIEW,uri))
+        var uri = Uri.parse("https://www.covid19india.org/")
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
 
     }
 
     fun openfeeds(view: View) {
-        var uri=Uri.parse("https://www.mohfw.gov.in/#latest-update")
-        startActivity(Intent(Intent.ACTION_VIEW,uri))
+        var uri = Uri.parse("https://www.mohfw.gov.in/#latest-update")
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
     }
 }
